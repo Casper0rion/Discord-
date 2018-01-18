5907d9d6ef9c04fdb85e59efcf2d0fd403567ac0
Glitch (discord-bot-host) <none>
2018-01-18 19:06:37 +0000

    🍉🏗 Checkpoint Discord bot commands.js:639446/2 package.json:639446/28 server.js:639446/4 index.js:639446/1

---
+++ index.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const token = 'Mzk3NjA0NjYxMjIzMzU4NDc0.DUJY2A.uykv_RJpJKf4q0Rglx5vaUfy_9U'
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+client.login('token');

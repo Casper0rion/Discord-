4d1392a748d77674896800707de50d598baef6cd
Glitch (discord-bot-host) <none>
2018-01-18 19:47:41 +0000

    📍🔖 Checkpoint public/client.js:639446/2 Discord bot commands.js:639446/347 index.js:639446/5

--- Discord bot commands.js
+++ Discord bot commands.js
@@ -11,20 +11,28 @@ const client = new Discord.Client();
 // The token of your bot - https://discordapp.com/developers/applications/me
 const token = 'Mzk3NjA0NjYxMjIzMzU4NDc0.DUJY2A.uykv_RJpJKf4q0Rglx5vaUfy_9U';
 
-// The ready event is vital, it means that your bot will only start reacting to information
-// from Discord _after_ ready is emitted
+
 client.on('ready', () => {
   console.log('I am ready!');
 });
 
-// Create an event listener for messages
+
 client.on('message', message => {
-  // If the message is "ping"
+ 
   if (message.content === 'ping') {
-    // Send "pong" to the same channel
     message.channel.send('pong');
   }
 });
 
-// Log our bot in
+
 client.login(token);
+
+
+
+
+
+// Create an event listener for messages
+  // Send "pong" to the same channel
+// Log our bot in
+// The ready event is vital, it means that your bot will only start reacting to information
+// from Discord _after_ ready is emitted

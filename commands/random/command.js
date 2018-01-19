7cd9c2b258fd44a012077fdd95ca7752b83c34b9
Glitch (discord-bot-host) <none>
2018-01-19 17:47:43 +0000

    🎹🎑 Checkpoint commands/random/command:639446/44 commands/random/command.js:639446/448

---
+++ commands/random/command.js
@@ -0,0 +1,20 @@
+const commando = require ("discord.js-commando")
+
+class DiceRollCommand extends commando.Command {
+  constructor(client){
+    super(client, {
+      name: 'roll',
+      group: 'random',
+      memberName: 'roll',
+      description: 'Rolls A Dice'
+    });
+  }
+                          
+                          
+                          
+                          
+                          
+                          
+                          
+                  
+                          

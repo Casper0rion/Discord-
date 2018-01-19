4880f55022d0d9f1b09d37fbf56e93e0ed440a03
Glitch (discord-bot-host) <none>
2018-01-19 17:26:28 +0000

    👑🏝 Checkpoint Discord Commando.js:639446/185 server.js:639446/11

--- Discord Commando.js
+++ Discord Commando.js
@@ -1,5 +1,11 @@
-const client = new commando.Client();
 const commando = require("discord.js-commando");
+const client = new commando.Client();
+const token = 'Mzk3NjA0NjYxMjIzMzU4NDc0.DUOu1w.bs4punNWQN3QL6MPcsLUp-lcdJc'
+
+bot 
 
 
 
+
+
+client.login(token)

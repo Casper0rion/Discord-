b79674cc93bd17f3f228bd6cab4bac139547b373
Glitch (discord-bot-host) <none>
2018-01-23 19:06:09 +0000

    😱🎬 Checkpoint server.js:639446/23

--- server.js
+++ server.js
@@ -24,4 +24,54 @@ client.on('message', msg => {
   }
 });
 
+client.on('message', message => {
+  // If the message is "what is my avatar"
+  if (message.content === '&avatar') {
+    // Send the user's avatar URL
+    message.reply(message.author.avatarURL);
+  }
+});
+
+// Create an event listener for new guild members
+client.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.find('name', 'member-log');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Welcome to the server, ${member}`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 client.login(token);

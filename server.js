de6161a56a15baf7ba626bfff3f53106f7daa37b
Glitch (discord-bot-host) <none>
2018-01-24 17:21:44 +0000

    🖇🏅 Checkpoint server.js:639446/60

--- server.js
+++ server.js
@@ -2,11 +2,22 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const token = 'Mzk3NjA0NjYxMjIzMzU4NDc0.DUOu1w.bs4punNWQN3QL6MPcsLUp-lcdJc'
 
+var online = false;
+
+
+
+
+
 
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
+
+function alive()
+{
+    online = false;
+}
 
 client.on('message', msg => {
   if (msg.content === '&ping') {

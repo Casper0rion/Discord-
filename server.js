d11be49dd935fca291b6cccc71da706df0c4c069
Glitch (discord-bot-host) <none>
2018-01-31 17:43:04 +0000

    🚉🎋 Checkpoint server.js:639446/7 devlog/webpage:639446/25

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require('./config.json');
-
+const { prefix, token } = require('./config.json');
 
 // This section is completly used for Var properties only
 //REMINDER somethings may be in the settings file

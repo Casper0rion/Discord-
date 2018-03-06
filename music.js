d46539bcd9f32bfd1460b59c77216711ac177240
Tyler <31228718+Casper0rion@users.noreply.github.com>
2018-03-06 12:26:14 -0500

    Added music command

--- music.js
+++ music.js
@@ -1,3 +1,25 @@
 const ytdl = require("ytdl-core");
 const Discord = require("discord.js");
 const client = new Discord.Client();
+
+client.on(`message`, message => {
+  if (message.content == `&play radio`) {
+    if (message.channel.type !== `text`) return;
+
+    const { voiceChannel } = message.member;
+
+    if (!voiceChannel) {
+      return message.reply(`You must join a voice channel!`);
+    }
+
+    voiceChannel.join().then(connection => {
+      const stream = ytdl(`inseert music here`, {
+        filter: `audioonly`
+      });
+
+      const dispatcher = connection.playstream(stream);
+
+      dispatcher.on(`end`, () => voiceChannel.leave());
+    });
+  }
+});

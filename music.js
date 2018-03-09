07ed9c347b60fee2bb00c4f888c196a549ebed48
Tyler <31228718+Casper0rion@users.noreply.github.com>
2018-03-09 12:00:07 -0500

    Huge packet update And Music reformat prep

--- music.js
+++ music.js
@@ -1,6 +1,7 @@
 const ytdl = require("ytdl-core");
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const music = require(`discord.js-musicbot-addon-v2`);
 
 var token = ``;
 
@@ -8,25 +9,6 @@ process.on(`unhandledRejection`, error =>
   console.error(`Uncaught Promise Rejection:\n ${error}`)
 );
 
-client.on(`message`, message => {
-  if (message.content == `&play radio`) {
-    if (message.channel.type !== `text`) return;
+const music = new music(client, {});
 
-    const { voiceChannel } = message.member;
-
-    if (!voiceChannel) {
-      return message.reply(`You must join a voice channel!`);
-    }
-
-    voiceChannel.join().then(connection => {
-      const stream = ytdl(`inseert music here`, {
-        filter: `audioonly`
-      });
-
-      const dispatcher = connection.playstream(stream);
-
-      dispatcher.on(`end`, () => voiceChannel.leave());
-    });
-  }
-});
 client.login(token);

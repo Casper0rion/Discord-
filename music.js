455a57bb7c5bfc22a0509e8f02f10a2de59310d2
Tyler <31228718+Casper0rion@users.noreply.github.com>
2018-03-15 10:55:03 -0500

    Music login added

--- music.js
+++ music.js
@@ -4,6 +4,10 @@ const client = new Discord.Client();
 
 var token = ``;
 
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
 process.on(`unhandledRejection`, error =>
   console.error(`Uncaught Promise Rejection:\n ${error}`)
 );
